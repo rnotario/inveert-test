171e784c81f0da46b97db68c40a516e4b993434b
Roberto Notario <notariorob@gmail.com>
2020-07-29 22:43:02 +0200

    Updates

---
+++ precache-manifest.d755a94d5444c1b02578668a9b4fff6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5098752b73db04cc2f283c13dc11f5",
+    "url": "/inveert-test/index.html"
+  },
+  {
+    "revision": "d09f31098bbcc860ee92",
+    "url": "/inveert-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e78d9ccea1b94b3e8f5",
+    "url": "/inveert-test/static/css/main.6573aad7.chunk.css"
+  },
+  {
+    "revision": "d09f31098bbcc860ee92",
+    "url": "/inveert-test/static/js/2.0a3cda7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/inveert-test/static/js/2.0a3cda7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e78d9ccea1b94b3e8f5",
+    "url": "/inveert-test/static/js/main.08d5dddf.chunk.js"
+  },
+  {
+    "revision": "03201736ae6e41dfaf27",
+    "url": "/inveert-test/static/js/runtime-main.f727f94b.js"
+  },
+  {
+    "revision": "ed80093ffb655dc098286437fbc4b0e5",
+    "url": "/inveert-test/static/media/sadFace.ed80093f.png"
+  }
+]);
