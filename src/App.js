0e6cae58a022bb91caad3b35a1a8e252debc2e15
Roberto Notario <notariorob@gmail.com>
2020-07-29 18:42:32 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import UserList from "./pages/UserList";
+import UserDetail from "./pages/UserDetail";
+
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Route path="/" component={UserList} exact />
+      <Route path="/users/:id" component={UserDetail} />
+    </Router>
   );
 }
 
